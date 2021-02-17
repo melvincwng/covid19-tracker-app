ff900039c2d2245e38e81dd29bfc08092a91db4d
melvincwng <melvincwng@users.noreply.github.com>
2021-02-17 13:55:58 +0800

    Work in progress to implement CountryPicker component

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { FormControl, NativeSelect } from '@material-ui/core';
+import styles from './CountryPicker.module.css'
+import { fetchCountries } from './../../api/index';
+
+function CountryPicker ({ handleSelectedCountry }) {
+
+}
+
+export default CountryPicker;
