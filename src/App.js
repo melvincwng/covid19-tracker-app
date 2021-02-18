36fa615a17dc608975401be7d9deacb93748e15d
melvincwng <melvincwng@users.noreply.github.com>
2021-02-18 15:11:47 +0800

    Implement logic for handleSelectedCountry function

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import { fetchData } from './api/index';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
 
 function App() {
 
   const [data, setData] = useState({});
-  const [country, setCountry] = useState();
+  const [country, setCountry] = useState("");
 
   // useEffect will automatically run after the components here are first rendered onto the screen.
   useEffect(() => {
     async function fetchMyAPI() {
-      let data_object = await fetchData();
-      setData(data_object)
+      const data_object = await fetchData();
+      setData(data_object) //setData(...) will re-render the App component again with the new data value
     }
     fetchMyAPI()
   },[])
+
+  //implementing logic for the handleSelectedCountry function here
+  //what handleSelectedCountry does is that it takes into a parameter country
+  //and then a) fetches the data for that specific country to b) update 1) cards component & 2) chart component
+  async function handleSelectedCountry(country) {
+    const country_data_object = await fetchData(country);
+    setData(country_data_object);
+    setCountry(country);
+  }
 
   return (
     <div className="App"> 
@@ -27,7 +37,7 @@ function App() {
         lastUpdate={data.lastUpdate}
         country={country}
         />
-      <CountryPicker /> 
+      <CountryPicker handleSelectedCountry={handleSelectedCountry}/> 
       <Chart 
         confirmed={data.confirmed}
         recovered={data.recovered}
@@ -38,4 +48,4 @@ function App() {
   );
 }
 
-export default App; //still need to implement logic for the handleSelectedCountry function here
+export default App; 
