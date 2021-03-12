056ce01574167f47b39b9601c6e303a341041c25
melvincwng <melvincwng@users.noreply.github.com>
2021-03-12 16:45:58 +0800

    Implement Loader component & its CSS

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect }  from 'react';
 import styles from './About.module.css';
 import axios from 'axios';
+import Loader from "react-loader-spinner";
 
 const url = "https://covid19-tracker-app-express.herokuapp.com/about";
 
 function About(){
-    const [aboutData, setAboutData ] = useState([]);
+    const [ aboutData, setAboutData ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false);
 
     const fetchAboutProject = async () => {
         try {
             const data = await axios.get(url); //data is an object here, and inside it, it has a key/property called 'data'. And data.data will give us an array, [{title: ..., body: ..., body1: ... etcs}]
+            await setIsLoading(false);
             return [data.data[0].title, data.data[0].body, data.data[0].body2, data.data[0].body3, data.data[0].body4]
         } catch (err) {
             console.log(err)
@@ -18,6 +21,7 @@ function About(){
 
     useEffect(() => {
         async function fetchMyAPI() {
+            await setIsLoading(true);
             let data_array = await fetchAboutProject();
             setAboutData(data_array)
         }
@@ -29,9 +33,9 @@ function About(){
     const body2 = aboutData[2];
     const body3 = aboutData[3];
     const body4 = aboutData[4];
-    
-    return(
-        <div className={styles.container}>
+
+    const aboutProject = 
+        <div>
             <h1>{title}</h1>
             <div>{body}</div>
             <br></br>
@@ -40,6 +44,12 @@ function About(){
             <div>{body3}</div>
             <br></br>
             <div>{body4}</div>
+        </div>
+
+    
+    return(
+        <div className={styles.container}>
+            { isLoading ? <div className={styles.loader}><Loader type="TailSpin" color="black" height={80} width={80} /></div> : aboutProject}
         </div>
     );
 }
