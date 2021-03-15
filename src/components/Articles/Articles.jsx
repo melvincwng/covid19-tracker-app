c4f68f8463e312f3b7506c859edd6f3677cef37a
melvincwng <melvincwng@users.noreply.github.com>
2021-03-15 16:10:01 +0800

    Implement Loader for Articles Component

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -6,11 +6,13 @@ import Loader from "react-loader-spinner";
 const url = "https://covid19-tracker-app-express.herokuapp.com/articles";
 
 function Articles() {
-    const [ articles, setArticles ] = useState([])
+    const [ articles, setArticles ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false);
 
     const fetchAllArticles = async () => {
         try {
             const articles = await axios.get(url); //articles is an object with 'data' property/attribute
+            await setIsLoading(false);
             return articles.data //articles.data is an array of articles
         } catch (err) {
             console.log(err)
@@ -19,6 +21,7 @@ function Articles() {
 
     useEffect(() => {
         async function fetchMyAPI() {
+            await setIsLoading(true);
             let articles_array = await fetchAllArticles();
             setArticles(articles_array);
         }
@@ -26,11 +29,21 @@ function Articles() {
     }, []); 
 
     //article.postDate will return a date string.
-    // To convert it into a readable data, refer to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse &  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    // To convert the date string into a readable date, refer to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse &  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    const articlesContainer = articles.map((article) => 
+            <div>
+                <h1>{article.title}</h1>
+                {article.body}
+                <br></br>
+                {article.authorName}
+                <br></br>
+                {(new Date(article.postDate)).toLocaleDateString('en-GB')}
+            </div>
+    );
     
     return(
         <div className={styles.container}>
-            {articles.map((article) => <div>{article.title} {article.body} {article.authorName} {(new Date(article.postDate)).toLocaleDateString('en-GB')}</div>)}
+            { isLoading ? <div className={styles.loader}><Loader type="TailSpin" color="black" height={80} width={80} /></div> : articlesContainer}
         </div>
     );
 }
