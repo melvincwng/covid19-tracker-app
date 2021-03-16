5c6d3569dd1d8570a0451f827f3a877c85e7350d
melvincwng <melvincwng@users.noreply.github.com>
2021-03-16 18:05:48 +0800

    Implement code which allows LoginForm component to send POST requests to backend & hence allowing user to login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from './LoginForm.module.css';
+import axios from 'axios';
+
+const url = 'https://covid19-tracker-app-express.herokuapp.com/users/login';
 
 function LoginForm() {
+    const [ disabled, setDisabled ] = useState(false); //to prevent users from clicking multiple times
 
     return (
         <Formik
@@ -17,10 +21,21 @@ function LoginForm() {
                 .min(8, 'Your password must have at least 8 characters.'),
             })}
             onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
+                async function fetchMyAPI() {
+                    try {
+                        const loginOutcome = await axios.post(url, values)
+                        alert(loginOutcome.data);
+                        window.location.href = '/' //after clicking 'OK' on alert box, it redirects user to home-page, refer to link for more info: https://stackoverflow.com/questions/33622057/redirect-after-alert-box
+                    } catch (err) {
+                        // Refer to this link for more info on err.response: https://stackoverflow.com/questions/39153080/how-can-i-get-the-status-code-from-an-http-error-in-axios
+                        // Main idea, when we receive a response from the server (that contains an error object), the error object will contain the response property, that has the data we need
+                        alert(err.response.data);
+                        setDisabled(false);
+                    }
+                }
+                setDisabled(true);
+                fetchMyAPI();
                 setSubmitting(false);
-                }, 400);
             }}
         >
             <Form className={styles.form}>
@@ -37,7 +52,7 @@ function LoginForm() {
                     {msg => <div className={styles.errorMessage}>{msg}</div>}
                 </ErrorMessage>
         
-                <button type="submit" className={styles.button}>Log In</button>
+                <button type="submit" disabled={disabled} className={styles.button}>Log In</button>
             </Form>
      </Formik>
     );
