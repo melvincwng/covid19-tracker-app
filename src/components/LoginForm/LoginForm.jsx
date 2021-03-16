8fc9c1dacf6198985ce0f2d94e207d8cbb0bfc37
melvincwng <melvincwng@users.noreply.github.com>
2021-03-16 12:44:46 +0800

    Implement LoginForm Component & its CSS code

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styles from './LoginForm.module.css';
 
 function LoginForm() {
 
     return (
-        <div>
-            
-        </div>
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            validationSchema={Yup.object({
+                username: Yup.string()
+                .required('This field is required.')
+                .min(3, 'Your username must have at least 3 characters.'),
+                password: Yup.string()
+                .required('This field is required.')
+                .min(8, 'Your password must have at least 8 characters.'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                }, 400);
+            }}
+        >
+            <Form className={styles.form}>
+                <h1>Login</h1>
+                
+                <Field name="username" type="text" placeholder="Username" className={styles.fieldOne}/>
+                <ErrorMessage name="username">
+                    {msg => <div className={styles.errorMessage}>{msg}</div>}
+                </ErrorMessage>
+        
+                
+                <Field name="password" type="text" placeholder="Password" className={styles.fieldTwo}/>
+                <ErrorMessage name="password">
+                    {msg => <div className={styles.errorMessage}>{msg}</div>}
+                </ErrorMessage>
+        
+                <button type="submit" className={styles.button}>Log In</button>
+            </Form>
+     </Formik>
     );
 }
 
