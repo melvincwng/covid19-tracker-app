7bdecaf2b61a10b1f4829bf75b3d2e9dc7d2f469
melvincwng <melvincwng@users.noreply.github.com>
2021-03-18 21:31:42 +0800

    Fix bug that causes cookie to not be cleared. Essentially there was a syntax problem with axios.post()

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -11,7 +11,16 @@ function Logout() {
 
     async function fetchMyAPI() {
         try {
-            const logoutOutcome = await axios.post(url, { withCredentials: true });
+            // actual syntax of axios.post is axios.post(url, data, config) where url is a string, data & configs are objects.
+            // what I observed is that for most axios requests, the 'config' object is usually optional
+            // hence in this case, the most important for axios post request is to have axios.post(url, data)
+            // In our case, we need the config object '{ withCredentials: true }' so that our axios requests are sent together with our cookie
+            // but the thing is if we just type axios.post(url, { withCredentials: true });
+            // what I believe the system sees it as is as axios.post(url, data). 
+            // Hence system thinks we are posting a 'data' object { withCredentials: true }... but that's not the case
+            // and a result, eventually it will lead to a bug where the cookie cannot be cleared when I send a post req to that url
+            // To solve that bug, instead, I add in the 'data' object as null -> then our cookie get cleared!
+            const logoutOutcome = await axios.post(url, null, { withCredentials: true }); 
             alert(logoutOutcome.data);
             window.location.href = '/';
             setDisabled(false);
