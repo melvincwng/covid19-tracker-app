2b979955616435184360ad27f4de72c678d63d9d
melvincwng <melvincwng@users.noreply.github.com>
2021-03-18 11:53:40 +0800

    Implement Admin feature in NavigationBar Component, if user is logged in

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -9,7 +9,7 @@ function NavigationBar() {
     // then it will look at the UserContext component in app.js and the value of the 'value' attribute will be returned
     // & stored in the const/variable at line 11
     const { user, setUser }= useContext(UserContext);  // object destructuring done here
-
+    
     return(
       <Navbar bg="transparent" expand="lg" className={styles.navbar}>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -17,6 +17,7 @@ function NavigationBar() {
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             { user ? <Nav.Link href="/logout">Logout</Nav.Link> : <Nav.Link href="/login">Login</Nav.Link> }
+            { user && <Nav.Link href="/admin">Admin</Nav.Link> }
             <Nav.Link href="/articles">Articles</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
           </Nav>
