d3a3a72c604f35b1a1e2d13bf7aa69ebe7a1b401
melvincwng <melvincwng@users.noreply.github.com>
2021-03-23 16:11:42 +0800

    Implement button to DELETE posts in Articles section

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect, useContext }  from 'react';
 import styles from './Articles.module.css';
 import axios from 'axios';
 import Loader from "react-loader-spinner";
+import { UserContext } from './../../UserContext';
 
 const url = "https://covid19-tracker-app-express.herokuapp.com/articles";
 
 function Articles() {
     const [ articles, setArticles ] = useState([]);
     const [ isLoading, setIsLoading ] = useState(false);
+    const { user, setUser } = useContext(UserContext);
 
     const fetchAllArticles = async () => {
         try {
@@ -28,6 +30,22 @@ function Articles() {
         fetchMyAPI();
     }, []); 
 
+    async function handleDelete(e) {
+        try {
+            e.preventDefault();
+            let buttonElement = e.target;
+            buttonElement.disabled = true; //cannot use the usual way of const [ disabled, setDisabled ] = useState(false) => because once you click a button, and setDisabled(true) => it will cause not only that button you click to be disabled, but also all the other delete buttons to be disabled as well.
+            let articleID = e.target.value;
+            const deleteUrl = `https://covid19-tracker-app-express.herokuapp.com/articles/${articleID}`;
+            await axios.delete(deleteUrl, { withCredentials: true });
+            alert('Article deleted!')
+            buttonElement.disabled = false;
+            window.location.href = '/articles'
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     //article.postDate will return a date string.
     // To convert the date string into a readable date, refer to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse &  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
 
@@ -37,7 +55,8 @@ function Articles() {
                 <div className={styles.limitText}>{article.body}</div>
                 <div className={styles.fontsize}>{article.authorName}</div>
                 <div className={styles.fontsize}>{(new Date(article.postDate)).toLocaleDateString('en-GB')}</div>
-                <a href={`/articles/${article._id}`} target="_blank" rel="noopener noreferrer"><button>Read more</button></a>
+                <a href={`/articles/${article._id}`} target="_blank" rel="noopener noreferrer"><button className={styles.button}>Read more</button></a>
+                { user &&  <button type="submit" value={article._id} onClick={handleDelete} className={styles.button}>Delete</button> }
                 <br></br>
                 <br></br> 
             </div>
