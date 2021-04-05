e7b42b07fc37ed68b5eede832619745a351c04a9
melvincwng <melvincwng@users.noreply.github.com>
2021-04-05 13:53:36 +0800

    Improve test coverage for Chart component

--- src/components/Chart/Chart.test.js
+++ src/components/Chart/Chart.test.js
@@ -1,7 +1,36 @@
 import Chart from './Chart';
 import { render } from '@testing-library/react';
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+const mockAxios = new MockAdapter(axios);
 
-test('should render and display a bar-chart on the screen', () => {
+describe("Chart component", () => {
+  //chartData is the response.data which we will get after the request
+  const chartData = [
+    {
+      confirmed: {total: 100},
+      deaths: {total: 50},
+      reportDate: "2020-01-22"
+    },
+  ]
+
+  beforeEach(() => {
+    mockAxios.reset();
+  });
+
+  it("should render the Chart component", async () => {
+    mockAxios
+      .onGet("https://covid19.mathdro.id/api/daily")
+      .reply(200, chartData);
+
+    const { getByTestId } = render(<Chart />);
+
+    expect(getByTestId("testing-chart")).toBeInTheDocument();
+  });
+});
+
+// Old tests below:
+/*test('should render and display a bar-chart on the screen', () => {
     const { getByTestId } = render(
         <Chart 
         confirmed={{value:100}}
@@ -23,4 +52,5 @@ test('should render and display a bar-chart on the screen', () => {
         );
     expect(getByTestId('testing-chart')).toBeInTheDocument();
   })
+*/
 
