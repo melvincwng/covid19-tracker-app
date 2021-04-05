63f1237094ae9881927641126e4126f30788e7e2
melvincwng <melvincwng@users.noreply.github.com>
2021-04-05 12:17:31 +0800

    Add additional test for Chart component

--- src/components/Chart/Chart.test.js
+++ src/components/Chart/Chart.test.js
@@ -1,13 +1,24 @@
 import Chart from './Chart';
 import { render } from '@testing-library/react';
 
-test('should render and display a chart on the screen', () => {
+test('should render and display a bar-chart on the screen', () => {
     const { getByTestId } = render(
         <Chart 
         confirmed={{value:100}}
         recovered={{value:70}}
         deaths={{value:20}}
         country="test country"
+        />
+        );
+    expect(getByTestId('testing-chart')).toBeInTheDocument();
+  })
+
+  test('should render and display a line-chart on the screen', () => {
+    const { getByTestId } = render(
+        <Chart 
+        confirmed={{value:100}}
+        recovered={{value:70}}
+        deaths={{value:20}}
         />
         );
     expect(getByTestId('testing-chart')).toBeInTheDocument();
