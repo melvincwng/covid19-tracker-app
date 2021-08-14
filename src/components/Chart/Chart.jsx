c1e3ae3abdf33e7979026c7389bf4319b82010f0
melvincwng <melvincwng@users.noreply.github.com>
2021-08-14 12:19:09 +0800

    Edit code in Chart.jsx since JHU CSSE is no longer maintaining data for recovered/active cases

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,171 +1,185 @@
-import React, { useState, useEffect } from 'react';
-import { fetchDailyData } from '../../api';
-import { Line, Bar } from 'react-chartjs-2';
-import styles from './Chart.module.css';
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
-function Chart ({ confirmed, recovered, deaths, country }) {
+function Chart({ confirmed, recovered, deaths, country }) {
+  const [dailyData, setDailyData] = useState([]);
 
-    const [dailyData, setDailyData] = useState([]);
-    
-    useEffect(() => {
-        async function fetchMyAPI() {
-            let daily_data_array = await fetchDailyData();
-            setDailyData(daily_data_array);
-        }
-        fetchMyAPI();
-    }, []);
-
-    // Visit react-chart-js on github to view and get a sense of how they implement their graphs
-    // Can see their source codes on how they implemented their line or bar charts.
-    const line_data = {
-        labels: dailyData.map((dailyData) => dailyData.date),
-        datasets: [
-            {
-                label: 'Infected',
-                data: dailyData.map((dailyData) => (dailyData.confirmed)),
-                fill: true,
-                borderColor:'rgb(0, 0, 255)'
-            },
-            {
-                label: 'Deaths',
-                data: dailyData.map((dailyData) => (dailyData.deaths)),
-                fill: true,
-                backgroundColor: 'rgba(255, 75, 100, 0.5)',
-                borderColor:'rgb(255, 0, 0)'
-            },
-        ],
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let daily_data_array = await fetchDailyData();
+      setDailyData(daily_data_array);
     }
-    
-    const line_options = {
-        scales: {
-          yAxes: [
-            {
-                ticks: {
-                    userCallback: function(value, index, values) {
-                        return value.toLocaleString();   // this helps to add commas to y-axis
-                    },
-                    fontFamily: 'Pangolin',
-                }
+    fetchMyAPI();
+  }, []);
+
+  // Visit react-chart-js on github to view and get a sense of how they implement their graphs
+  // Can see their source codes on how they implemented their line or bar charts.
+  const line_data = {
+    labels: dailyData.map((dailyData) => dailyData.date),
+    datasets: [
+      {
+        label: "Infected",
+        data: dailyData.map((dailyData) => dailyData.confirmed),
+        fill: true,
+        borderColor: "rgb(0, 0, 255)",
+      },
+      {
+        label: "Deaths",
+        data: dailyData.map((dailyData) => dailyData.deaths),
+        fill: true,
+        backgroundColor: "rgba(255, 75, 100, 0.5)",
+        borderColor: "rgb(255, 0, 0)",
+      },
+    ],
+  };
+
+  const line_options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            userCallback: function (value, index, values) {
+              return value.toLocaleString(); // this helps to add commas to y-axis
             },
-          ],
-          xAxes: [{ticks: {fontFamily:'Pangolin'}}],
+            fontFamily: "Pangolin",
+          },
         },
+      ],
+      xAxes: [{ ticks: { fontFamily: "Pangolin" } }],
+    },
 
-        //According to chartjs official docs, "the label callback can change the text that displays on the
-        // screen for a given data point". In this case, this section is code is applicable since I wish
-        // to format the number displayed in each tooltip to contain the comma separator every 3 digits.
-        // the 'data' parameter in line 58 actually refers to line_data (try to understand the logic)
-        // cause later on, in line 59, we will use line_data to access the datasets key/property
-        // line_data.datasets is actually an array containing two objects (1 obj at index 0, 1 obj at index 1)
-        // the object at index 0 of the array aka line_data.datasets[0] will have many tooltips, 
-        // each individually attached to a datapoint in data (line_data.datasets[0].data).
-        // This is the same logic for the object at index 1 of the array line_data.datasets
-        // hence tooltipItem.datasetIndex means what is its dataset index of the tooltipItem (see below)
-        // aka does the tooltip item belong to dataset index 0 (line_data.datasets[0]) or dataset index 1 (line_data.datasets[1])
-        // then subsequently once we know whether the tooltipItem's datasetIndex is 0 or 1 then
-        // we access the data property which is a mapped array with many values where...
-        // each tooltipItem will be assigned an individual value in this mapped array and hence have an index
-        // hence in line 73 where we ...data[tooltipItem.index] => we will get tooltipValue
-        // to grab the label in line 72 is the same logic as above & .toLocaleString() helps to format a number with commas.
-        
-        tooltips: {
-            callbacks: {
-                label: function (tooltipItem, data) { 
-                    let label = data.datasets[tooltipItem.datasetIndex].label
-                    let tooltipValue = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
-                    return `${label}: ${tooltipValue.toLocaleString()}`; 
-                }
-              }
-            },
+    //According to chartjs official docs, "the label callback can change the text that displays on the
+    // screen for a given data point". In this case, this section is code is applicable since I wish
+    // to format the number displayed in each tooltip to contain the comma separator every 3 digits.
+    // the 'data' parameter in line 58 actually refers to line_data (try to understand the logic)
+    // cause later on, in line 59, we will use line_data to access the datasets key/property
+    // line_data.datasets is actually an array containing two objects (1 obj at index 0, 1 obj at index 1)
+    // the object at index 0 of the array aka line_data.datasets[0] will have many tooltips,
+    // each individually attached to a datapoint in data (line_data.datasets[0].data).
+    // This is the same logic for the object at index 1 of the array line_data.datasets
+    // hence tooltipItem.datasetIndex means what is its dataset index of the tooltipItem (see below)
+    // aka does the tooltip item belong to dataset index 0 (line_data.datasets[0]) or dataset index 1 (line_data.datasets[1])
+    // then subsequently once we know whether the tooltipItem's datasetIndex is 0 or 1 then
+    // we access the data property which is a mapped array with many values where...
+    // each tooltipItem will be assigned an individual value in this mapped array and hence have an index
+    // hence in line 73 where we ...data[tooltipItem.index] => we will get tooltipValue
+    // to grab the label in line 72 is the same logic as above & .toLocaleString() helps to format a number with commas.
 
-        legend: {
-            labels: { 
-                fontFamily: 'Pangolin'
-            }
-        }
-      };
-    
-    // the variable const bar_data (line 60 - 91) has been causing me a lot of bugs
-    // Long story short when the Chart component is first rendered, data is an empty object
-    // hence data.confirmed will be undefined and undefined is being passed in as props to Charts component
-    // which you cannot get the value property of an undefined item 
-    // That is what the error kept saying cannot read value property of undefined
-    // hence to fix this, have to use ternary operator (see logic below)
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          let label = data.datasets[tooltipItem.datasetIndex].label;
+          let tooltipValue =
+            data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+          return `${label}: ${tooltipValue.toLocaleString()}`;
+        },
+      },
+    },
 
-    const bar_data = confirmed ? {
+    legend: {
+      labels: {
+        fontFamily: "Pangolin",
+      },
+    },
+  };
+
+  // the variable const bar_data (line 60 - 91) has been causing me a lot of bugs
+  // Long story short when the Chart component is first rendered, data is an empty object
+  // hence data.confirmed will be undefined and undefined is being passed in as props to Charts component
+  // which you cannot get the value property of an undefined item
+  // That is what the error kept saying cannot read value property of undefined
+  // hence to fix this, have to use ternary operator (see logic below)
+
+  const bar_data = confirmed
+    ? {
         labels: ["Infected", "Recovered", "Active", "Deaths"],
         datasets: [
-            {
-                label: "People",
-                data: [
-                    confirmed.value, // this was the part causing errors (line 64)
-                    recovered.value, 
-                    confirmed.value - recovered.value - deaths.value, 
-                    deaths.value
-                ],
-                backgroundColor: [
-                    'rgba(0, 0, 255, 0.6)',
-                    'rgba(0, 255, 0, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(255, 0, 0, 0.6)'
-                ],
-                borderColor: [
-                    'rgba(0, 0, 255, 1)',
-                    'rgba(0, 255, 0, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 0, 0, 1)'
-                ],
-                borderWidth: 1,
-                hoverBackgroundColor: [
-                    'rgba(0, 80, 160)',
-                    'rgba(30, 100, 50)',
-                    'rgba(200, 150, 0)',
-                    'rgba(255, 50, 50)'
-                ], 
-            },
-        ],
-    } : null // this should fix the bug
-
-    // this options variable is for the bar-charts
-    // can refer to https://www.chartjs.org/docs/latest/configuration/title.html for more info:
-    const bar_options = {
-        title: { display: true, text: `Current situation in ${country}`, fontFamily:'Pangolin'},
-        scales: {
-            yAxes: [
-              {
-                  ticks: {
-                      userCallback: function(value, index, values) {
-                          return value.toLocaleString();   // this helps to convert the add commas to y-axis
-                      },
-                      fontFamily: 'Pangolin',
-                  }
-              },
+          {
+            label: "People",
+            data: [
+              confirmed.value, // this was the part causing errors (line 64)
+              0, // originally this line was 'recovered.value' but since JHU CSSE is no longer maintaining recovered/active data, this was removed & changed to 0
+              0, // originally this line was 'confirmed.value - recovered.value - deaths.value' but since JHU CSSE is no longer maintaining recovered/active data, this was removed & changed to 0
+              deaths.value,
             ],
-            xAxes: [{ticks: {fontFamily:'Pangolin'}}],
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.6)",
+              "rgba(0, 255, 0, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(255, 0, 0, 0.6)",
+            ],
+            borderColor: [
+              "rgba(0, 0, 255, 1)",
+              "rgba(0, 255, 0, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(255, 0, 0, 1)",
+            ],
+            borderWidth: 1,
+            hoverBackgroundColor: [
+              "rgba(0, 80, 160)",
+              "rgba(30, 100, 50)",
+              "rgba(200, 150, 0)",
+              "rgba(255, 50, 50)",
+            ],
           },
-        tooltips: {
-            callbacks: {
-                label: function (tooltipItem, data) {  // data parameter refers to bar_data
-                    let label = data.datasets[tooltipItem.datasetIndex].label
-                    let tooltipValue = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
-                    return `${label}: ${tooltipValue.toLocaleString()}`;
-                }
-              }
-            },
-        legend: {
-            labels: { 
-                fontFamily: 'Pangolin'
-            }
-        }
-    }
+        ],
+      }
+    : null; // this should fix the bug
 
-    const lineChart = dailyData.length ? <Line data={line_data} options={line_options}></Line> : null;
-    const barChart = confirmed ? <Bar data={bar_data} options={bar_options}></Bar>: null; 
-    // generate bar chart if confirmed is not an empty object (aka like {value: some-num, type: example})
-    
-    return (
-        <div className={styles.container} data-testid="testing-chart">{ country ? barChart : lineChart }</div>
-    );
+  // this options variable is for the bar-charts
+  // can refer to https://www.chartjs.org/docs/latest/configuration/title.html for more info:
+  const bar_options = {
+    title: {
+      display: true,
+      text: `Current situation in ${country}`,
+      fontFamily: "Pangolin",
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            userCallback: function (value, index, values) {
+              return value.toLocaleString(); // this helps to convert the add commas to y-axis
+            },
+            fontFamily: "Pangolin",
+          },
+        },
+      ],
+      xAxes: [{ ticks: { fontFamily: "Pangolin" } }],
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          // data parameter refers to bar_data
+          let label = data.datasets[tooltipItem.datasetIndex].label;
+          let tooltipValue =
+            data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+          return `${label}: ${tooltipValue.toLocaleString()}`;
+        },
+      },
+    },
+    legend: {
+      labels: {
+        fontFamily: "Pangolin",
+      },
+    },
+  };
+
+  const lineChart = dailyData.length ? (
+    <Line data={line_data} options={line_options}></Line>
+  ) : null;
+  const barChart = confirmed ? (
+    <Bar data={bar_data} options={bar_options}></Bar>
+  ) : null;
+  // generate bar chart if confirmed is not an empty object (aka like {value: some-num, type: example})
+
+  return (
+    <div className={styles.container} data-testid="testing-chart">
+      {country ? barChart : lineChart}
+    </div>
+  );
 }
 
 export default Chart;
