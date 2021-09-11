f07c618ee364e73d3a529042cd2e602fddbfcf68
melvincwng <melvincwng@users.noreply.github.com>
2021-09-12 02:47:27 +0800

    Add in image upload functionality for front-end & backend

--- src/components/Articles/IndividualArticle.jsx
+++ src/components/Articles/IndividualArticle.jsx
@@ -1,47 +1,63 @@
 import React, { useState, useEffect } from "react";
-import styles from './IndividualArticle.module.css'
-import axios from 'axios';
+import styles from "./IndividualArticle.module.css";
+import axios from "axios";
 import Loader from "react-loader-spinner";
 
 function IndividualArticle(props) {
-    const articleID = props.match.params.id;
-    const url = `https://covid19-tracker-app-express.herokuapp.com/articles/${articleID}`;
-    const [ article, setArticle ] = useState({});
-    const [ isLoading, setIsLoading ] = useState(false);
+  const articleID = props.match.params.id;
+  const url = `https://covid19-tracker-app-express.herokuapp.com/articles/${articleID}`;
+  const [article, setArticle] = useState({});
+  const [imageBaseUrl, setImageBaseUrl] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
 
-    const fetchIndividualArticle = async () => {
-      try {
-        const article = await axios.get(url); //article is an object here with 'data' attribute => article.data gives us the whole article object
-        await setIsLoading(false);
-        return article.data //article.data gives us the whole article object
-      } catch (err) {
-        console.log(err)
+  const fetchIndividualArticle = async () => {
+    try {
+      const article = await axios.get(url); //article is an object here with 'data' attribute => article.data gives us the whole article object
+      await setIsLoading(false);
+      return article.data; //article.data gives us the whole article object
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      await setIsLoading(true);
+      const articleObject = await fetchIndividualArticle();
+      setArticle(articleObject);
+
+      // Some articles will not have articleImages... while some will have. Hence need to put if statement here...
+      if (articleObject.articleImage) {
+        setImageBaseUrl(`https://covid19-tracker-app-express.herokuapp.com/images/${articleObject.articleImage}`);
       }
     }
-
-    useEffect(() => {
-      async function fetchMyAPI() {
-        await setIsLoading(true);
-        const articleObject = await fetchIndividualArticle();
-        setArticle(articleObject);
-      }
-      fetchMyAPI();
+    fetchMyAPI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  }, []);
 
-    const singleArticleContainer = 
-      <div>
-        <h3>{article.title}</h3>
-        <div className={styles.whitespace}>{article.body}</div>
-        <div className={styles.fontsize}>{article.authorName}</div>
-        <div className={styles.fontsize}>{(new Date(article.postDate)).toLocaleDateString('en-GB')}</div>
+  const singleArticleContainer = (
+    <div>
+      <h3>{article.title}</h3>
+      <div className={styles.whitespace}>{article.body}</div>
+      {imageBaseUrl === "" ? "" : <img src={imageBaseUrl} alt="" className={styles.image}></img>}
+      <div className={styles.fontsize}>{article.authorName}</div>
+      <div className={styles.fontsize}>
+        {new Date(article.postDate).toLocaleDateString("en-GB")}
       </div>
+    </div>
+  );
 
-    return (
-        <div className={styles.container}>
-          { isLoading ? <div className={styles.loader}><Loader type="TailSpin" color="black" height={80} width={80} /></div> : singleArticleContainer}
+  return (
+    <div className={styles.container}>
+      {isLoading ? (
+        <div className={styles.loader}>
+          <Loader type="TailSpin" color="black" height={80} width={80} />
         </div>
-      );
+      ) : (
+        singleArticleContainer
+      )}
+    </div>
+  );
 }
 
 export default IndividualArticle;
