f6e9a8353097b5ead03af84e7d2811e456af0985
melvincwng <melvin.ng@outlook.sg>
2022-08-11 21:15:43 +0800

    Increase NavigationBar.test.js test coverage

--- src/components/NavigationBar/NavigationBar.test.js
+++ src/components/NavigationBar/NavigationBar.test.js
@@ -1,15 +1,29 @@
 import NavigationBar from "./NavigationBar";
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
 import { UserContext } from "../../UserContext";
 
-test('should render and display a navigation bar on the screen', () => {
-    const value = 'valid-User-For-Testing-Purposes';
+test("If user is not logged in, it should just render and display a navigation bar on the screen", () => {
+  const value = "null-User-For-Testing-Purposes";
 
-    const { getByTestId } = render(
-        <UserContext.Provider value={value}>
-            <NavigationBar />
-        </UserContext.Provider>
-        );
+  const { getByTestId } = render(
+    <UserContext.Provider value={value}>
+      <NavigationBar />
+    </UserContext.Provider>
+  );
 
-    expect(getByTestId('test-navbar')).toBeInTheDocument();
-})
+  expect(getByTestId("test-navbar")).toBeInTheDocument();
+});
+
+test("If user is logged in, it should render and display a navigation bar on the screen with 'Logout' & 'Admin' text", () => {
+  const value = { user: true, setUser: jest.fn() };
+
+  const { getByTestId } = render(
+    <UserContext.Provider value={value}>
+      <NavigationBar />
+    </UserContext.Provider>
+  );
+
+  expect(getByTestId("test-navbar")).toBeInTheDocument();
+  expect(getByTestId("test-navbar").textContent).toContain("Logout");
+  expect(getByTestId("test-navbar").textContent).toContain("Admin");
+});
