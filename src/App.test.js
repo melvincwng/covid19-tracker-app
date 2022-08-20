568929718389ba0158694c7899c6a14e3ba05a5e
melvincwng <melvin.ng@outlook.sg>
2022-08-20 16:47:27 +0800

    Improve unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
-import App from './App';
-import { render } from '@testing-library/react';
+import React from "react";
+import App from "./App";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 
-test("should render & display various components of App", () => {
-  const value = 'valid-User-For-Testing-Purposes';
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('test-app')).toBeInTheDocument();
+test("should render App component", () => {
+  const { getByText, getByTestId, debug } = render(<App />);
+  expect(getByTestId("test-app")).toBeInTheDocument();
 });
-  
+
+test("should App component in various mocked states", () => {
+  // Arrange
+  const mockUseState = jest.spyOn(React, "useState");
+  mockUseState
+    .mockImplementationOnce(() => [{}, jest.fn()])
+    .mockImplementationOnce(() => ["testCountry", jest.fn()])
+    .mockImplementationOnce(() => [true, jest.fn()]);
+  const mockUseMemo = jest.spyOn(React, "useMemo");
+  mockUseMemo.mockImplementationOnce(() => ({
+    user: true,
+    setUser: jest.fn(),
+  }));
+  const windowSpy = jest.spyOn(window, "open");
+
+  // Act
+  const { getByText, getByTestId, debug } = render(<App />);
+  fireEvent.click(getByTestId("github-icon"));
+  fireEvent.click(getByTestId("itch-icon"));
+  fireEvent.click(getByTestId("coffee-icon"));
+
+  console.log(getByTestId("coffee-icon").getAttribute(""));
+  // Assert
+  expect(getByTestId("test-app")).toBeInTheDocument();
+  expect(getByTestId("github-icon")).toBeInTheDocument();
+  expect(getByTestId("itch-icon")).toBeInTheDocument();
+  expect(getByTestId("coffee-icon")).toBeInTheDocument();
+  expect(windowSpy).toHaveBeenCalledTimes(3);
+  // debug();
+});
