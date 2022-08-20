b82386cf9678d86cbef1d5f624f0688a5170c511
melvincwng <melvin.ng@outlook.sg>
2022-08-20 19:37:51 +0800

    Increase unit test coverage to >= 80% overall

--- src/components/Chart/Chart.test.js
+++ src/components/Chart/Chart.test.js
@@ -1,18 +1,19 @@
-import Chart from './Chart';
-import { render } from '@testing-library/react';
+import Chart from "./Chart";
+import { render } from "@testing-library/react";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import React from "react";
 const mockAxios = new MockAdapter(axios);
 
 describe("Chart component", () => {
   //chartData is the response.data which we will get after the request
   const chartData = [
     {
-      confirmed: {total: 100},
-      deaths: {total: 50},
-      reportDate: "2020-01-22"
+      confirmed: { total: 100 },
+      deaths: { total: 50 },
+      date: "2020-01-22",
     },
-  ]
+  ];
 
   beforeEach(() => {
     mockAxios.reset();
@@ -27,30 +28,20 @@ describe("Chart component", () => {
 
     expect(getByTestId("testing-chart")).toBeInTheDocument();
   });
+
+  it("should render the Chart component again but containing a line chart", () => {
+    mockAxios
+      .onGet("https://covid19.mathdro.id/api/daily")
+      .reply(200, chartData);
+    const props = {
+      confirmed: { value: 50 },
+      deaths: { value: 10 },
+      country: undefined,
+    };
+
+    const { getByTestId, debug } = render(<Chart {...props} />);
+
+    //debug();
+    expect(getByTestId("testing-chart")).toBeInTheDocument();
+  });
 });
-
-// Old tests below:
-/*test('should render and display a bar-chart on the screen', () => {
-    const { getByTestId } = render(
-        <Chart 
-        confirmed={{value:100}}
-        recovered={{value:70}}
-        deaths={{value:20}}
-        country="test country"
-        />
-        );
-    expect(getByTestId('testing-chart')).toBeInTheDocument();
-  })
-
-  test('should render and display a line-chart on the screen', () => {
-    const { getByTestId } = render(
-        <Chart 
-        confirmed={{value:100}}
-        recovered={{value:70}}
-        deaths={{value:20}}
-        />
-        );
-    expect(getByTestId('testing-chart')).toBeInTheDocument();
-  })
-*/
-
