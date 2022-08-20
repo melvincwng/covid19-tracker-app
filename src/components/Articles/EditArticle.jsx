b6fcc54d63cc5c6dfe8248c6731d476cc0efaaf2
melvincwng <melvin.ng@outlook.sg>
2022-08-20 14:37:51 +0800

    Increase unit test coverage for EditArticle.jsx

--- src/components/Articles/EditArticle.jsx
+++ src/components/Articles/EditArticle.jsx
@@ -1,92 +1,139 @@
 import React, { useState, useEffect } from "react";
-import styles from './EditArticle.module.css'
-import axios from 'axios';
+import styles from "./EditArticle.module.css";
+import axios from "axios";
 import Loader from "react-loader-spinner";
 
 function EditArticle(props) {
-    const articleID = props.match.params.id;
-    const url = `https://covid19-tracker-app-express.herokuapp.com/articles/${articleID}`;
-    const [ article, setArticle ] = useState({});
-    const [ isLoading, setIsLoading ] = useState(false);
-    const [ disabled, setDisabled ] = useState(false);
+  const articleID = props.match.params.id;
+  const url = `https://covid19-tracker-app-express.herokuapp.com/articles/${articleID}`;
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
-    const fetchIndividualArticle = async () => {
-        try {
-          const article = await axios.get(url); //article is an object here with 'data' attribute => article.data gives us the whole article object
-          await setIsLoading(false);
-          return article.data //article.data gives us the whole article object
-        } catch (err) {
-          console.log(err)
-        }
-      }
-  
-    useEffect(() => {
-        async function fetchMyAPI() {
-            await setIsLoading(true);
-            const articleObject = await fetchIndividualArticle();
-            setArticle(articleObject);
-        }
-        fetchMyAPI();
+  const fetchIndividualArticle = async () => {
+    try {
+      const article = await axios.get(url); //article is an object here with 'data' attribute => article.data gives us the whole article object
+      await setIsLoading(false);
+      return article.data; //article.data gives us the whole article object
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      await setIsLoading(true);
+      const articleObject = await fetchIndividualArticle();
+      setArticle(articleObject);
+    }
+    fetchMyAPI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  }, []);
 
-    function handleSubmit(e) {
-        async function fetchMyAPI() {
-            try {
-                await axios.put(url, convertedFormData, { withCredentials: true });
-                alert('Article edited!');
-                setDisabled(false);
-                window.location.href = '/articles'
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        e.preventDefault();
-        setDisabled(true);
-        let formElement = document.getElementById("form");
-        let formData = new FormData(formElement);
-        //There was a bug here whereby formData object was not working apparently with axios.put() request... 
-        //When fetchMyAPI() was called -> originally was axios.put(url, formData, { withCredentials: true }) -> However, what happened here was that even request was handled successfully, the response (article) returned was the old article and not the newly updated one...(bug)
-        //Instead of using a FormData object, I tried replacing it with a JS object instead e.g. { title: "test", body:"test", authorName: "test" } -> it solved the bug & the article does update accordingly
-        //Hence for that rationale, I implemented lines 54-57 to convert the FormData object (line 49) to a JS object, then put it in the axios.put() request instead of the formData object
-        let convertedFormData = {} 
-        for (let [key, value] of formData.entries()) { 
-            convertedFormData[key] = value
-        }
-        fetchMyAPI(); 
+  function handleSubmit(e) {
+    async function fetchMyAPI() {
+      try {
+        await axios.put(url, convertedFormData, { withCredentials: true });
+        alert("Article edited!");
+        setDisabled(false);
+        window.location.href = "/articles";
+      } catch (err) {
+        console.log(err);
+      }
     }
-    
-    function handleTitleChange(e) {
-        setArticle(article => ({ ...article, title: e.target.value}))
+    e.preventDefault();
+    setDisabled(true);
+    let formElement = document.getElementById("form");
+    let formData = new FormData(formElement);
+    //There was a bug here whereby formData object was not working apparently with axios.put() request...
+    //When fetchMyAPI() was called -> originally was axios.put(url, formData, { withCredentials: true }) -> However, what happened here was that even request was handled successfully, the response (article) returned was the old article and not the newly updated one...(bug)
+    //Instead of using a FormData object, I tried replacing it with a JS object instead e.g. { title: "test", body:"test", authorName: "test" } -> it solved the bug & the article does update accordingly
+    //Hence for that rationale, I implemented lines 54-57 to convert the FormData object (line 49) to a JS object, then put it in the axios.put() request instead of the formData object
+    let convertedFormData = {};
+    for (let [key, value] of formData.entries()) {
+      convertedFormData[key] = value;
     }
+    fetchMyAPI();
+  }
 
-    function handleBodyChange(e) {
-        setArticle(article => ({ ...article, body: e.target.value}))
-    }
+  function handleTitleChange(e) {
+    setArticle((article) => ({ ...article, title: e.target.value }));
+  }
 
-    function handleNameChange(e) {
-        setArticle(article => ({ ...article, authorName: e.target.value}))
-    }
+  function handleBodyChange(e) {
+    setArticle((article) => ({ ...article, body: e.target.value }));
+  }
 
-    const singleArticleContainer = 
-        <form className={styles.form} id="form" encType="multipart/form-data" onSubmit={handleSubmit} data-testid="edit-article-form">
-            <h3>Edit article</h3>
-            <label for="title">Title:</label>
-            <input type="text" id="title" name="title" value={article.title} onChange={handleTitleChange} placeholder="Add a catchy title..." required></input>
-            <label for="body" className={styles.spacing}>Body:</label>
-            <textarea type="text" id="body" name="body" value={article.body} onChange={handleBodyChange} placeholder="Write your article here..." required></textarea>
-            <label for="authorName" className={styles.spacing}>Author Name:</label>
-            <input type="text" id="authorName" name="authorName" value={article.authorName} onChange={handleNameChange} placeholder="Your name..." required></input>
-            <input type="submit" value="Update" disabled={disabled} className={styles.button}></input>            
-        </form>
+  function handleNameChange(e) {
+    setArticle((article) => ({ ...article, authorName: e.target.value }));
+  }
 
-    return (
-        <div className={styles.container}>
-          { isLoading ? <div className={styles.loader}><Loader type="TailSpin" color="black" height={80} width={80} /></div> : singleArticleContainer}
+  const singleArticleContainer = (
+    <form
+      className={styles.form}
+      id="form"
+      encType="multipart/form-data"
+      onSubmit={handleSubmit}
+      data-testid="edit-article-form"
+    >
+      <h3>Edit article</h3>
+      <label for="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={article.title}
+        onChange={handleTitleChange}
+        placeholder="Add a catchy title..."
+        required
+        data-testid="edit-title"
+      ></input>
+      <label for="body" className={styles.spacing}>
+        Body:
+      </label>
+      <textarea
+        type="text"
+        id="body"
+        name="body"
+        value={article.body}
+        onChange={handleBodyChange}
+        placeholder="Write your article here..."
+        required
+        data-testid="edit-body"
+      ></textarea>
+      <label for="authorName" className={styles.spacing}>
+        Author Name:
+      </label>
+      <input
+        type="text"
+        id="authorName"
+        name="authorName"
+        value={article.authorName}
+        onChange={handleNameChange}
+        placeholder="Your name..."
+        required
+        data-testid="edit-author-name"
+      ></input>
+      <input
+        type="submit"
+        value="Update"
+        disabled={disabled}
+        className={styles.button}
+      ></input>
+    </form>
+  );
+
+  return (
+    <div className={styles.container}>
+      {isLoading ? (
+        <div className={styles.loader}>
+          <Loader type="TailSpin" color="black" height={80} width={80} />
         </div>
-    );
+      ) : (
+        singleArticleContainer
+      )}
+    </div>
+  );
 }
 
 export default EditArticle;
-
-//disabled={disabled}
--- src/components/Articles/EditArticle.test.js
+++ src/components/Articles/EditArticle.test.js
@@ -1,41 +1,49 @@
-import EditArticle from './EditArticle';
-import { render, waitFor } from '@testing-library/react';
+import EditArticle from "./EditArticle";
+import { render, waitFor, fireEvent } from "@testing-library/react";
 import { UserContext } from "../../UserContext";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 const mockAxios = new MockAdapter(axios);
 
 describe("should render and display the EditArticle component on the screen", () => {
-    const value = 'valid-User-For-Testing-Purposes';
+  const value = "valid-User-For-Testing-Purposes";
 
-    const individualArticleData = 
-        {
-            title: "Test Article",
-            body: "This is an article made for testing purposes",
-            authorName: "Test Author - Jim",
-        }
-    ;
-  
-    beforeEach(() => {
-      mockAxios.reset();
+  const individualArticleData = {
+    title: "Test Article",
+    body: "This is an article made for testing purposes",
+    authorName: "Test Author - Jim",
+  };
+  beforeEach(() => {
+    mockAxios.reset();
+  });
+
+  it("should render the EditArticle component & simulate submission of article edits", async () => {
+    mockAxios
+      .onGet(
+        "https://covid19-tracker-app-express.herokuapp.com/articles/605ad60b78d1010015f1337f"
+      )
+      .reply(200, individualArticleData);
+
+    const { getByTestId } = render(
+      <UserContext.Provider value={value}>
+        <EditArticle match={{ params: { id: "605ad60b78d1010015f1337f" } }} />
+      </UserContext.Provider>
+    );
+
+    await waitFor(() => getByTestId("edit-article-form"));
+    await waitFor(() => getByTestId("edit-title"));
+    await waitFor(() => getByTestId("edit-body"));
+    await waitFor(() => getByTestId("edit-author-name"));
+    fireEvent.submit(getByTestId("edit-article-form"));
+    fireEvent.change(getByTestId("edit-title"), {
+      target: { value: "testData" },
     });
-    
-    it("should render the EditArticle component", async () => {
-        
-        mockAxios
-            .onGet("https://covid19-tracker-app-express.herokuapp.com/articles/605ad60b78d1010015f1337f")
-            .reply(200, individualArticleData);
-        
-            const { getByTestId } = render(
-                <UserContext.Provider value={value}>
-                    <EditArticle match={{ params: {id: "605ad60b78d1010015f1337f"} }}/>
-                </UserContext.Provider>
-            );
-  
-        await waitFor(() => getByTestId("edit-article-form"));
-
-        expect(getByTestId('edit-article-form')).toBeInTheDocument();
-        
+    fireEvent.change(getByTestId("edit-body"), {
+      target: { value: "testData" },
     });
-
+    fireEvent.change(getByTestId("edit-author-name"), {
+      target: { value: "testData" },
+    });
+    expect(getByTestId("edit-article-form")).toBeInTheDocument();
+  });
 });
