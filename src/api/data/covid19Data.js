dcfdc19fe66b65bdf7e24cd0342cbdf613842a6c
melvincwng <melvin.ng@outlook.sg>
2023-12-26 22:40:52 +0800

    Update covid19 stats as per dec 2023

--- src/api/data/covid19Data.js
+++ src/api/data/covid19Data.js
@@ -1,8 +1,8 @@
 export const covid19GlobalDataForCards = {
-  confirmed: { value: 699620241 },
-  recovered: { value: 669393550 },
-  deaths: { value: 6956560 },
-  lastUpdate: "2023-12-16T22:00:00",
+  confirmed: { value: 700461781 },
+  recovered: { value: 671667307 },
+  deaths: { value: 6961081 },
+  lastUpdate: "2023-12-26T22:00:00",
 };
 
 export const covid19GlobalDataForChart = [
@@ -52,15 +52,18 @@ export const covid19GlobalDataForChart = [
   { confirmed: 691894642, deaths: 6903530, date: "2023-07-18" },
   { confirmed: 693664611, deaths: 6908560, date: "2023-08-18" },
   { confirmed: 695581217, deaths: 6918250, date: "2023-09-20" },
+  { confirmed: 697006249, deaths: 6938643, date: "2023-10-20" },
+  { confirmed: 698614835, deaths: 6950967, date: "2023-11-25" },
+  { confirmed: 700380256, deaths: 6960890, date: "2023-12-25" },
 ];
 
 export const covid19CountriesArray = ["Singapore"];
 
 export const covid19SingaporeDataForCards = {
-  confirmed: { value: 2771072 },
+  confirmed: { value: 2827115 },
   recovered: { value: 2707350 },
   deaths: { value: 1933 },
-  lastUpdate: "2023-12-16T22:00:00",
+  lastUpdate: "2023-12-26T22:00:00",
 };
 
 export const covid19SingaporeDataForChart = [
@@ -111,5 +114,5 @@ export const covid19SingaporeDataForChart = [
   { confirmed: 2560028, deaths: 1875, date: "2023-09-20" },
   { confirmed: 2679066, deaths: 1906, date: "2023-10-31" },
   { confirmed: 2752768, deaths: 1933, date: "2023-11-29" },
-  { confirmed: 2771072, deaths: 1933, date: "2023-12-15" },
+  { confirmed: 2827115, deaths: 1933, date: "2023-12-26" },
 ];
